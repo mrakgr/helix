13cc82956e9879d505608ed54f66b0ad063192bc
Marko Grdinić <mrakgr@gmail.com>
2023-05-24 15:00:27 +0200

    Adios Bolero, welcome Blazor.

---
+++ Nodes/TextNode.cs
@@ -0,0 +1,14 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+namespace Helix.Nodes;
+
+public class TextNode : NodeModel
+{
+    // This should be immutable, but nwm that for now.
+    public required string Text { get; set; }
+
+    public TextNode(Point p) : base(p)
+    {
+    }
+}

74c955e8eda2b1ea9568f35c7ade65a849c923a6
Marko Grdinić <mrakgr@gmail.com>
2023-05-25 14:48:21 +0200

    Time to bring back F#.

---
+++ Types/CompilationNode.cs
@@ -0,0 +1,14 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+namespace Helix.Nodes;
+
+public class CompilationNode : NodeModel
+{
+    // This should be immutable, but nwm that for now.
+    public required string Text { get; set; }
+    
+    public CompilationNode(Point p) : base(p)
+    {
+    }
+}
---
+++ Types/HelixPort.cs
@@ -0,0 +1,26 @@
+﻿using Blazor.Diagrams.Core.Models;
+
+namespace Helix.Nodes;
+
+public class HelixPort : PortModel
+{
+    public HelixPort(NodeModel parent, PortAlignment alignment, bool isInput) : base(parent, alignment, null, null)
+    {
+        IsInput = isInput;
+    }
+
+    public bool IsInput { get; set; }
+
+    public override bool CanAttachTo(PortModel port)
+    {
+        // Checks for same-node/port attachements
+        if (!base.CanAttachTo(port))
+            return false;
+
+        // Only able to attach to the same port type
+        if (!(port is HelixPort cp))
+            return false;
+
+        return IsInput != cp.IsInput;
+    }
+}
